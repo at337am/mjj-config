940fccd44e08cd62c0a7fc330de4ec7501b262b6
at337am <lueqit@gmail.com>
2025-08-01 11:26:02 +0800

    feat(zsh): 新增 alias lt

--- zsh/.lain/lib/aliases.zsh
+++ zsh/.lain/lib/aliases.zsh
@@ -11,6 +11,7 @@ alias bd='bat -d'
 alias bp='bat -p'
 alias l='eza --long --all --binary --time-style="+%y%m%d %H:%M"'
 alias lf='l --only-files'
+alias lt='l --tree'
 alias fm='dolphin . > /dev/null 2>&1'
 
 # base
