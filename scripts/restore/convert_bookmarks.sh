e0b50028ad64d2f6901ef2c7ccb56235aa3c685f
at337am <lueqit@gmail.com>
2025-08-26 01:49:50 +0800

    update convert_bookmarks.sh

--- scripts/restore/convert_bookmarks.sh
+++ scripts/restore/convert_bookmarks.sh
@@ -2,10 +2,10 @@
 
 bookmarks_path="$HOME/Documents/notes/other/bookmarks.md"
 
-output_dir="$HOME/Downloads/bookmarks_converted"
+output_dir="$HOME/Documents"
 
 md2pg "$bookmarks_path" --output-dir "$output_dir"
 
-cp -a "$output_dir/bookmarks.html" "$HOME/Documents/"
+cp -a "$output_dir/bookmarks.html" "$HOME/Downloads/"
 
 printf "Conversion Successful!\n"
