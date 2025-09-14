2526117219bc9c0367aece7be64f1f2a7ad556c1
at337am <lueqit@gmail.com>
2025-09-14 15:48:12 +0800

    WIP

--- rofi/scripts/edit-config.sh
+++ rofi/scripts/edit-config.sh
@@ -8,11 +8,11 @@ fi
 declare -A configs
 configs["hypr"]="$HOME/.config/hypr"
 configs["rofi"]="$HOME/.config/rofi"
+configs["waybar"]="$HOME/.config/waybar"
 configs["kitty"]="$HOME/.config/kitty"
 configs["rime"]="$HOME/.local/share/fcitx5/rime"
-configs["waybar"]="$HOME/.config/waybar"
 
-config_choice=$(printf "%s\n" "${!configs[@]}" | rofi -dmenu -i -p "Edit File")
+config_choice=$(printf "%s\n" "${!configs[@]}" | rofi -dmenu -i -p "Edit Config")
 
 # 如果按 Esc 退出，则脚本结束
 if [[ -z "$config_choice" ]]; then
@@ -22,6 +22,6 @@ fi
 # 获取所选文件对应的真实路径
 file_path=${configs[$config_choice]}
 
-code "$file_path" --ozone-platform-hint=auto > /dev/null 2>&1 &
+command code "$file_path" --ozone-platform-hint=auto > /dev/null 2>&1 &
 
 # kitty -e nvim "$file_path"
