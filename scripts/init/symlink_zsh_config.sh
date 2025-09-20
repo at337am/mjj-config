e4d54534579608c0de92aef7b7ad0166cc69b9fd
at337am <lueqit@gmail.com>
2025-09-21 01:43:11 +0800

    WIP

--- scripts/init/symlink_zsh_config.sh
+++ scripts/init/symlink_zsh_config.sh
@@ -6,18 +6,24 @@ command rm -rf ~/.zsh_history && \
 command rm -rf ~/.zcompdump && \
 command rm -rf ~/.lain && \
 command rm -rf ~/.cache/p10k*
+echo "zsh 旧配置清理完成"
 
-rsync -avh ~/workspace/dev/mjj-config/zsh/ ~/
+rsync -a ~/workspace/dev/mjj-config/zsh/ ~/
+echo "zsh 配置同步完成"
 
 (cd ~/.lain/themes && \
 tar -zxf powerlevel10k.tar.gz && \
 command rm -f powerlevel10k.tar.gz)
+echo "zsh p10k 主题解压完成"
 
 chmod 600 ~/.zshrc &&; \
 chmod 600 ~/.zprofile; \
 chmod 600 ~/.p10k.zsh
+echo "zsh 文件权限设置完成"
 
 command rm -rf ~/.lain/lib/aliases.zsh && \
 ln -s ~/workspace/dev/mjj-config/zsh/.lain/lib/aliases.zsh ~/.lain/lib/aliases.zsh
+echo "zsh aliases.zsh 链接设置完成"
 
 chsh -s /usr/bin/zsh
+echo "zsh 已设置为默认 shell"
