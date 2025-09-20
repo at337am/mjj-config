3102ca216e07a14ef34f0d0f2948e6d52e9dd74e
at337am <lueqit@gmail.com>
2025-09-21 07:21:24 +0800

    WIP

--- scripts/init/setup_basic.sh
+++ scripts/init/setup_basic.sh
@@ -25,20 +25,3 @@ sudo mkdir -p /opt/soft /opt/venvs && \
 sudo chown -R $(whoami):$(id -gn) /opt/soft /opt/venvs
 
 echo "-=> 已创建 个人偏好 的目录结构 <=-"
-
-
-
-
-# todo, 修改 xdg-user-dirs-update 语言:
-
-# LANG=en_US.UTF-8 xdg-user-dirs-update --force && cat ~/.config/user-dirs.dirs
-
-# mv -n ~/下载/* ~/Downloads/; \
-# mv -n ~/文档/* ~/Documents/; \
-# mv -n ~/图片/* ~/Pictures/; \
-# mv -n ~/视频/* ~/Videos/; \
-# mv -n ~/音乐/* ~/Music/; \
-# mv -n ~/模板/* ~/Templates/; \
-# mv -n ~/公共/* ~/Public/; \
-# mv -n ~/桌面/* ~/Desktop/; \
-# command rm -rfv ~/下载 ~/文档 ~/图片 ~/视频 ~/音乐 ~/模板 ~/公共 ~/桌面
