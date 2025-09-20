99bd27bcc8b4f5021f1aa0a5be4e4fb6081d82d0
at337am <lueqit@gmail.com>
2025-09-21 01:25:03 +0800

    WIP

--- scripts/init/dnf_install.sh
+++ scripts/init/dnf_install.sh
@@ -2,21 +2,21 @@
 
 # 启动仓库
 if sudo dnf repolist | grep -q "alternateved:eza"; then
-    echo "===>>> eza copr 仓库已启用，无需重复操作 <<<==="
+    echo "--- eza copr 仓库已启用，无需重复操作 ---"
 else
-    echo "===>>> 正在启用 eza 的 copr 仓库... <<<==="
+    echo "--- 正在启用 eza 的 copr 仓库... ---"
     sudo dnf copr enable alternateved/eza
 fi
 
 # 启动仓库
 if sudo dnf repolist | grep -q "solopasha:hyprland"; then
-    echo "===>>> Hyprland copr 仓库已启用，无需重复操作 <<<==="
+    echo "--- Hyprland copr 仓库已启用，无需重复操作 ---"
 else
-    echo "===>>> 正在启用 Hyprland 的 copr 仓库... <<<==="
+    echo "--- 正在启用 Hyprland 的 copr 仓库... ---"
     sudo dnf copr enable solopasha/hyprland
 fi
 
-echo "===>>> 正在安装基础软件包... <<<==="
+echo "--- 正在安装基础软件包... ---"
 sudo dnf -y install \
     zsh \
     git \
@@ -45,7 +45,7 @@ sudo dnf -y install \
     navi \
     adb
 
-echo "===>>> 正在安装 Hyprland 及相关软件包... <<<==="
+echo "--- 正在安装 Hyprland 及相关软件包... ---"
 sudo dnf -y install \
     hyprland \
     hyprpaper \
@@ -67,7 +67,7 @@ sudo dnf -y install \
     mako \
     cmatrix
 
-echo "===>>> 正在安装 Breeze 光标主题... <<<==="
+echo "--- 正在安装 Breeze 光标主题... ---"
 sudo dnf -y install breeze-cursor-theme
 
 
