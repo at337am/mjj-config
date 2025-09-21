906472f9b7fd3dfa59643aa145a2f3954b5bdbe7
at337am <lueqit@gmail.com>
2025-09-21 08:44:18 +0800

    WIP

--- scripts/init/configs/rsync_config.sh
+++ scripts/init/configs/rsync_config.sh
@@ -15,8 +15,8 @@ echo "-=> gitconfig 文件权限已设置"
 rsync -a ~/workspace/dev/mjj-config/ssh/config ~/.ssh/
 echo "-=> ssh config 配置同步完成"
 
-chmod 700 ~/.ssh && \
-chmod 600 ~/.ssh/config && \
-chmod 600 ~/.ssh/id_rsa && \
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/config
+chmod 600 ~/.ssh/id_rsa
 chmod 644 ~/.ssh/id_rsa.pub
 echo "-=> ssh 文件权限已设置"
--- scripts/init/configs/symlink_config.sh
+++ scripts/init/configs/symlink_config.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-command rm -rf ~/.cache/fastfetch && \
-command rm -rf ~/.config/fastfetch && \
+command rm -rf ~/.cache/fastfetch
+command rm -rf ~/.config/fastfetch
 ln -s ~/workspace/dev/mjj-config/fastfetch ~/.config/fastfetch
 echo "-=> fastfetch 配置已链接"
 
-command rm -rf ~/.config/fd && \
+command rm -rf ~/.config/fd
 ln -s ~/workspace/dev/mjj-config/fd ~/.config/fd
 echo "-=> fd 配置已链接"
 
-command rm -rf ~/.config/hypr && \
+command rm -rf ~/.config/hypr
 ln -s ~/workspace/dev/mjj-config/hypr ~/.config/hypr
 echo "-=> hypr 配置已链接"
 
-command rm -rf ~/.config/kitty && \
+command rm -rf ~/.config/kitty
 ln -s ~/workspace/dev/mjj-config/kitty ~/.config/kitty
 echo "-=> kitty 配置已链接"
 
-command rm -rf ~/.config/mako && \
+command rm -rf ~/.config/mako
 ln -s ~/workspace/dev/mjj-config/mako ~/.config/mako
 echo "-=> mako 配置已链接"
 
-command rm -rf ~/.config/mpv && \
+command rm -rf ~/.config/mpv
 ln -s ~/workspace/dev/mjj-config/mpv ~/.config/mpv
 echo "-=> mpv 配置已链接"
 
-command rm -rf ~/.config/navi && \
+command rm -rf ~/.config/navi
 ln -s ~/workspace/dev/mjj-config/navi ~/.config/navi
 echo "-=> navi 配置已链接"
 
@@ -33,18 +33,18 @@ command rm -rf \
         ~/.config/nvim \
         ~/.local/share/nvim \
         ~/.local/state/nvim \
-        ~/.cache/nvim && \
+        ~/.cache/nvim
 ln -s ~/workspace/dev/mjj-config/nvim ~/.config/nvim
 echo "-=> nvim 配置已链接"
 
-command rm -rf ~/.config/rofi && \
+command rm -rf ~/.config/rofi
 ln -s ~/workspace/dev/mjj-config/rofi ~/.config/rofi
 echo "-=> rofi 配置已链接"
 
-command rm -rf ~/.config/waybar && \
+command rm -rf ~/.config/waybar
 ln -s ~/workspace/dev/mjj-config/waybar ~/.config/waybar
 echo "-=> waybar 配置已链接"
 
-command rm -rf ~/.config/yt-dlp && \
+command rm -rf ~/.config/yt-dlp
 ln -s ~/workspace/dev/mjj-config/yt-dlp ~/.config/yt-dlp
 echo "-=> yt-dlp 配置已链接"
