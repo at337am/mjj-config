a50fdd7c7bcd63efd34e234ab8ed1a153cc957f8
at337am <lueqit@gmail.com>
2025-09-21 08:43:12 +0800

    WIP

--- scripts/init/setup_basic.sh
+++ scripts/init/setup_basic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo systemctl stop firewalld && \
+sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 echo "-=> 防火墙已关闭 <=-"
 
@@ -14,14 +14,17 @@ echo "-=> 已将 sudo 过期时间设置为 60 分钟 <=-"
 echo "$(whoami) ALL=(ALL) NOPASSWD: /usr/sbin/shutdown" | sudo tee -a /etc/sudoers
 echo "-=> 已设置免密关机 <=-"
 
-sudo mkdir -p /workspace/dev /workspace/tmp && \
-sudo chown -R $(whoami):$(id -gn) /workspace && \
+mkdir -p ~/.config
+mkdir -p ~/.local/share
+
+sudo mkdir -p /workspace/dev /workspace/tmp
+sudo chown -R $(whoami):$(id -gn) /workspace
 ln -s /workspace ~/workspace
 
-sudo mkdir -p /data/bak/restore /data/hello /data/misc/tgboom && \
+sudo mkdir -p /data/bak/restore /data/hello /data/misc/tgboom
 sudo chown -R $(whoami):$(id -gn) /data
 
-sudo mkdir -p /opt/soft /opt/venvs && \
+sudo mkdir -p /opt/soft /opt/venvs
 sudo chown -R $(whoami):$(id -gn) /opt/soft /opt/venvs
 
-echo "-=> 已创建 个人偏好 的目录结构 <=-"
+echo "-=> 已创建相关目录结构 <=-"
