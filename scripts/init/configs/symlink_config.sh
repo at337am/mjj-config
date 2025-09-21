b034456e2b0bed1d3873a5939061fbe1f7c09e4d
at337am <lueqit@gmail.com>
2025-09-22 01:13:58 +0800

    WIP

--- scripts/init/configs/symlink_config.sh
+++ scripts/init/configs/symlink_config.sh
@@ -1,33 +1,40 @@
 #!/usr/bin/env bash
 
+# 设置严格模式，任何错误都会导致脚本退出
+set -euo pipefail
+
+log() {
+    echo "-=> $1 <=-"
+}
+
 command rm -rf ~/.cache/fastfetch
 command rm -rf ~/.config/fastfetch
 ln -s ~/workspace/dev/mjj-config/fastfetch ~/.config/fastfetch
-echo "-=> fastfetch 配置已链接"
+log "fastfetch 配置已链接"
 
 command rm -rf ~/.config/fd
 ln -s ~/workspace/dev/mjj-config/fd ~/.config/fd
-echo "-=> fd 配置已链接"
+log "fd 配置已链接"
 
 command rm -rf ~/.config/hypr
 ln -s ~/workspace/dev/mjj-config/hypr ~/.config/hypr
-echo "-=> hypr 配置已链接"
+log "hypr 配置已链接"
 
 command rm -rf ~/.config/kitty
 ln -s ~/workspace/dev/mjj-config/kitty ~/.config/kitty
-echo "-=> kitty 配置已链接"
+log "kitty 配置已链接"
 
 command rm -rf ~/.config/mako
 ln -s ~/workspace/dev/mjj-config/mako ~/.config/mako
-echo "-=> mako 配置已链接"
+log "mako 配置已链接"
 
 command rm -rf ~/.config/mpv
 ln -s ~/workspace/dev/mjj-config/mpv ~/.config/mpv
-echo "-=> mpv 配置已链接"
+log "mpv 配置已链接"
 
 command rm -rf ~/.config/navi
 ln -s ~/workspace/dev/mjj-config/navi ~/.config/navi
-echo "-=> navi 配置已链接"
+log "navi 配置已链接"
 
 command rm -rf \
         ~/.config/nvim \
@@ -35,16 +42,16 @@ command rm -rf \
         ~/.local/state/nvim \
         ~/.cache/nvim
 ln -s ~/workspace/dev/mjj-config/nvim ~/.config/nvim
-echo "-=> nvim 配置已链接"
+log "nvim 配置已链接"
 
 command rm -rf ~/.config/rofi
 ln -s ~/workspace/dev/mjj-config/rofi ~/.config/rofi
-echo "-=> rofi 配置已链接"
+log "rofi 配置已链接"
 
 command rm -rf ~/.config/waybar
 ln -s ~/workspace/dev/mjj-config/waybar ~/.config/waybar
-echo "-=> waybar 配置已链接"
+log "waybar 配置已链接"
 
 command rm -rf ~/.config/yt-dlp
 ln -s ~/workspace/dev/mjj-config/yt-dlp ~/.config/yt-dlp
-echo "-=> yt-dlp 配置已链接"
+log "yt-dlp 配置已链接"
