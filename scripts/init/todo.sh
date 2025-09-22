384e9e200b78be152d9b6ab0e75baab299ab3925
at337am <lueqit@gmail.com>
2025-09-22 21:20:50 +0800

    WIP

--- scripts/init/todo.sh
+++ scripts/init/todo.sh
@@ -58,6 +58,7 @@ command rm -rfv ~/下载 ~/文档 ~/图片 ~/视频 ~/音乐 ~/模板 ~/公共 ~
 # ------------- todo -------------
 
 # 安装后的 dnf 代理设置 脚本
+# 安装后的 xdg-user-dirs-update 语言
 
 # 脚本 执行前 可以先  sudo dnf autoremove
 
@@ -66,4 +67,3 @@ command rm -rfv ~/下载 ~/文档 ~/图片 ~/视频 ~/音乐 ~/模板 ~/公共 ~
 # 脚本 前面 增加 rpm fussion 检查  存在 仓库就不执行
 # setup_basic 也增加 检查  存在不执行
 # 还有其他脚本  文件都加上检查
-
