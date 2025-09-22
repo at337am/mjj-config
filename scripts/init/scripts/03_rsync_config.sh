7d1ccbdf2d2937709a5545afdb210ca8ac6495a6
at337am <lueqit@gmail.com>
2025-09-23 00:45:20 +0800

    WIP

--- scripts/init/scripts/03_rsync_config.sh
+++ scripts/init/scripts/03_rsync_config.sh
@@ -18,12 +18,12 @@ rsync -a ~/workspace/dev/mjj-config/gitconfig/.gitconfig ~/
 log "设置 gitconfig 文件权限..."
 chmod 600 ~/.gitconfig
 
-log "同步 ssh config 配置..."
-rsync -a ~/workspace/dev/mjj-config/ssh/config ~/.ssh/
-
-log "设置 ssh 文件权限..."
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/config
 # todo, 这里只是为了测试暂时不需要 ssh
-chmod 600 ~/.ssh/id_rsa || true
-chmod 644 ~/.ssh/id_rsa.pub || true
+# log "同步 ssh config 配置..."
+# rsync -a ~/workspace/dev/mjj-config/ssh/config ~/.ssh/
+
+# log "设置 ssh 文件权限..."
+# chmod 700 ~/.ssh
+# chmod 600 ~/.ssh/config
+# chmod 600 ~/.ssh/id_rsa
+# chmod 644 ~/.ssh/id_rsa.pub
