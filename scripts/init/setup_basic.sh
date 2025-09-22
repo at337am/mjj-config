70e6b40889e0dd60bf31f8342fab17eabc78b183
at337am <lueqit@gmail.com>
2025-09-23 00:33:27 +0800

    WIP

--- scripts/init/setup_basic.sh
+++ scripts/init/setup_basic.sh
@@ -94,25 +94,31 @@ log "解压完毕, 各文件已到位"
 
 
 # ------------- 执行脚本 START -------------- #
-scripts=(
-    "scripts/01_dnf_install.sh"
-    "scripts/02_rsync_zsh.sh"
-    "scripts/03_rsync_config.sh"
-    "scripts/04_symlink_config.sh"
-    "scripts/05_flatpak_install.sh"
-    "scripts/06_setup_flatpak_perm.sh"
-)
+SCRIPT_DIR="scripts"
 
-for script in "${scripts[@]}"; do
-    if [[ ! -f "$file" ]]; then
-        log "Error: 脚本 '$file' 不存在"
+if [[ ! -d "$SCRIPT_DIR" ]]; then
+    log "Error: 目录 '$SCRIPT_DIR' 不存在"
+    exit 1
+fi
+
+find "$SCRIPT_DIR" -maxdepth 1 -name "*.sh" | sort -V | while read -r script; do
+    if [[ ! -x "$script" ]]; then
+        log "Error: 脚本 '$script' 没有执行权限"
         exit 1
     fi
 
-    log "开始执行脚本 $script..."
+    log "开始执行脚本: $script"
+    
+    "$script"
 
-    ./"$script"
+    # 检查上一个命令（也就是执行的脚本）的退出码
+    # 如果退出码不为 0，表示执行失败
+    exit_code=$?
+    if [[ $exit_code -ne 0 ]]; then
+        log "Error: 脚本 '$script' 执行失败, 退出码: $exit_code"
+        exit 1
+    fi
 
-    log "执行完毕 $script"
+    log "脚本执行完毕: $script"
 done
 # ------------- 执行脚本 END -------------- #
